c18eab1b04476d945782bd83cd6403d2005dfd1d
Deo-EKodi <deoekodi@gmail.com>
2024-06-06 00:02:34 -0700

    terminal_putchar - prints valid characters to screen

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,4 @@
 #include "kernel.h"
-// #include <stdint.h>
 
 
 uint16_t* video_mem = 0;
@@ -11,7 +10,7 @@ void terminal_initialize(void)
     {
         for (int x = 0; x < VGA_WIDTH; ++x)
         {
-            video_mem[(y * VGA_WIDTH) + x] = terminal_uint16_t_make_char(' ', 0);
+            terminal_putchar(x, y, ' ', 0);
         }
     }
 }
@@ -21,7 +20,25 @@ uint16_t terminal_uint16_t_make_char(char c, char col)
     return (col << 8) | c;
 }
 
+size_t strlen(const char* c)
+{
+    size_t len = 0;
+    
+    /**
+     * hunt for null-terminator
+     */
+    while(c[len])
+    {
+        len++;
+    }
+    return len;
+}
 
+
+void terminal_putchar(int x, int y, char c, char color)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_uint16_t_make_char(c, color);
+}
 /**
  * 1st byte - character
  * 2nd byte - color
--- src/kernel.h
+++ src/kernel.h
@@ -2,9 +2,15 @@
 #define KERNEL_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 20
+
+// headers
+void terminal_putchar(int x, int y, char c, char color);
+size_t strlen(const char*);
+void terminal_initialize(void);
 void kernel_main(void);
 uint16_t terminal_uint16_t_make_char(char c, char col);
 

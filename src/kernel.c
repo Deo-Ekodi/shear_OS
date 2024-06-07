1d7f1311cec4fddffb535384e5d2366985096345
Deo-EKodi <deoekodi@gmail.com>
2024-06-07 04:56:55 -0700

    paging enabled

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "idt/idt.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -70,6 +71,8 @@ void print(const char* str)
     }
 }
 
+
+static struct paging_4gb_chunk* kernel_chunk = 0;
 void kernel_main()
 {
     terminal_initialize();
@@ -80,6 +83,13 @@ void kernel_main()
 
     // Initialize the interrupt descriptor table
     idt_init();
+
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE);
+
+// switch
+    paging_switch(paging_4b_chunk_get_directory(kernel_chunk));
+
+    enable_paging();
 //   enable interrupts
     enable_interrupts();
 };

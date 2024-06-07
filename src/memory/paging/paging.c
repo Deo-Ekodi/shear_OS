bb0457c11a9623d4cc21caf8963294c9eb42514b
Deo-EKodi <deoekodi@gmail.com>
2024-06-07 04:17:36 -0700

    setting up 4 gigs chunk

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -4,4 +4,8 @@
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags)
 {
     kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
+    for(int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; ++i)
+    {
+        uint32_t*entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
+    }
 }

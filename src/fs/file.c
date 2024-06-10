062041871703b1d4f3c9e92489d3d7e8bdedeccd
Deo-EKodi <deoekodi@gmail.com>
2024-06-10 08:54:45 -0700

    filesystem

---
+++ src/fs/file.c
@@ -0,0 +1,18 @@
+#include "file.h"
+
+void fs_init()
+{
+    ;
+}
+int fopen(const char*, const char*)
+{
+    ;
+}
+void fs_insert_filesystem(struct filesystem*)
+{
+    ;
+}
+struct filesystem* fs_resolve(struct disk*)
+{
+    ;
+}

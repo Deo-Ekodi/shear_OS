3c6f97d386e65c18cbc2f8fe6738f140c531da97
Deo-EKodi <deoekodi@gmail.com>
2024-06-11 04:05:23 -0700

    file_descriptor

--- src/fs/file.c
+++ src/fs/file.c
@@ -1,18 +1,89 @@
 #include "file.h"
+#include "config.h"
+#include "memory/memory.h"
+#include "status.h"
+#include "memory/heap/kheap.h"
 
-void fs_init()
+
+
+struct  filesystem* filesystems[SHEAROS_MAX_FILESYSTEMS];
+struct file_descriptor* file_descriptors[SHEAROS_MAX_FILE_DESCRIPTORS];
+
+static struct filesystem** fs_get_free_filesystem()
 {
-    ;
+    int i = 0;
+    for(; i < SHEAROS_MAX_FILESYSTEMS; ++i)
+    {
+        if(filesystems[i] == 0)
+        {
+            return &filesystems[i];
+        }
+    }
+
+    return 0;
 }
+
+;
+
+
 int fopen(const char*, const char*)
 {
     ;
 }
-void fs_insert_filesystem(struct filesystem*)
+void fs_insert_filesystem(struct filesystem* filesystem)
 {
-    ;
+    struct filesystem** fs;
+    fs = fs_get_free_filesystem();
+    if(!fs)
+    {
+        print("problem inserting filesystem.");
+        while(1){}
+    }
+
+    *fs = filesystem;
 }
-struct filesystem* fs_resolve(struct disk*)
+
+static void fs_static_load()
 {
-    ;
+    // fs_insert_filesysten(fat16_init());
 }
+
+void fs_load()
+{
+    memset(filesystems, 0, sizeof(filesystems));
+    fs_static_load();
+}
+
+void fs_init()
+{
+    memset(file_descriptors, 0, sizeof(file_descriptors));
+    fs_load();
+}
+
+static int file_new_descriptor(struct file_descriptor** desc_out)
+{
+    int res = -ENOMEM;
+    for(int i = 0; i < SHEAROS_MAX_FILE_DESCRIPTORS; ++i)
+    {
+        if(file_descriptors[i] == 0)
+        {
+            struct file_descriptor* desc = kzalloc(sizeof(struct file_descriptor));
+            desc->index = i + 1;
+            file_descriptors[i] = desc;
+            *desc_out = desc;
+            res = 0;
+            break;
+        }
+    }
+    return res;
+}
+
+static struct file_descriptor* file_get_descriptor(int fd)
+{
+
+}
+
+// struct filesystem* fs_resolve(struct disk* disk)
+// {
+//     ;
+// }

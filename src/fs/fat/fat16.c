4207d7a5362b9ca6c9f9b4c340fc92e9d2cbce77
Deo-EKodi <deoekodi@gmail.com>
2024-06-11 08:48:38 -0700

    fat16 -updated & broken

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -3,6 +3,26 @@
 #include "memory/memory.h"
 #include "status.h"
 #include "memory/heap/kheap.h"
+
+typedef unsigned int FAT_ITEM_TYPE;
+#define FAT_ITEM_TYPE_DIRECTORY 0
+#define FAT_ITEM_TYPE_FILE      1
+
+#define SHEAROS_FAT16_SIGNATURE         0x29
+#define SHEAROS_FAT16_FAT_ENTRY_SIZE    0x02
+#define SHEAROS_FAT16_BAD_SECTOR        0xFF7
+#define SHEAROS_FAT16_UNSIGNED          0x00
+
+// fat directories attributes bitmask
+#define FAT_FILE_READ_ONLY      0x01
+#define FAT_FILE_HIDDEN         0x02
+#define FAT_FILE_SYSTEM         0x04
+#define FAT_FILE_VOLUME_LABEL   0x08
+#define FAT_FILE_SUBDIRECTORY   0x10
+#define FAT_FILE_ARCHIVED       0x20
+#define FAT_FILE_DEVICE         0x40
+#define FAT_FILE_RESERVED       0x80
+
 struct fat_header_extended
 {
     uint8_t drive_number;
--- src/fs/fat/fat16.h
+++ src/fs/fat/fat16.h
@@ -8,29 +8,6 @@
 #include "disk/streamer.h"
 #include "disk/disk.h"
 
-#define SHEAROS_FAT16_SIGNATURE         0x29
-#define SHEAROS_FAT16_FAT_ENTRY_SIZE    0x02
-#define SHEAROS_FAT16_BAD_SECTOR        0xFF7
-#define SHEAROS_FAT16_UNSIGNED          0x00
-
-// fat directories attributes bitmask
-#define FAT_FILE_READ_ONLY      0x01
-#define FAT_FILE_HIDDEN         0x02
-#define FAT_FILE_SYSTEM         0x04
-#define FAT_FILE_VOLUME_LABEL   0x08
-#define FAT_FILE_SUBDIRECTORY   0x10
-#define FAT_FILE_ARCHIVED       0x20
-#define FAT_FILE_DEVICE         0x40
-#define FAT_FILE_RESERVED       0x80
-
-typedef unsigned int FAT_ITEM_TYPE;
-#define FAT_ITEM_TYPE_DIRECTORY 0
-#define FAT_ITEM_TYPE_FILE      1
-
-
-
-
-
 
 
 void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode);

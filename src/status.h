afccb63e7c9d4030cd9c445c0ccf806227ed64d9
Deo-EKodi <deoekodi@gmail.com>
2024-06-12 06:47:00 -0700

    fat16_sketch

--- src/status.h
+++ src/status.h
@@ -2,12 +2,24 @@
 #define STATUS_H
 
 #define SHEAROS_ALL_OK      0
+
+// Error Input/Output
 #define EIO                 1
+
+// Error Invalid Argument
 #define EINVARG             2
+
+// Error No Memory
 #define ENOMEM              3
+
+// Error Nad Path
 #define EBADPATH            4
+
+// Error FIlesystem Not Us
 #define EFSNOTUS            5
 
+// Error File Read Only
+#define ERDONLY             6
 
 
 

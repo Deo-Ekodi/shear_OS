3d9df1ee50e0eabd0295fa55b9140e990a39ba23
Deo-EKodi <deoekodi@gmail.com>
2024-06-16 01:08:29 -0700

    tasks

---
+++ src/task/task.c
@@ -0,0 +1,134 @@
+#include "task.h"
+#include "kernel.h"
+#include "status.h"
+#include "memory/paging/paging.h"
+#include "memory/heap/kheap.h"
+#include "memory/memory.h"
+
+/**
+ * currennt task that is running
+ */
+struct task* current_task = 0;
+
+/**
+ * task linked list head
+ */
+struct task* task_tail = 0;
+
+/**
+ * task linked list tail
+ */
+struct task* task_head = 0;
+
+struct task* task_current()
+{
+    return current_task;
+}
+
+int task_init(struct task* task);
+
+struct task* task_new()
+{
+    int res = 0;
+    struct task* task = kzalloc(sizeof(struct task));
+    if(!task)
+    {
+        res = -ENOMEM;
+        goto out;
+    }
+
+    res = task_init(task);
+    if(res != SHEAROS_ALL_OK)
+    {
+        goto out;
+    }
+
+    /**
+     * case empty linked list
+     */
+    if(task_head == 0)
+    {
+        task_head = task;
+        task_tail = task;
+        goto out;
+    }
+    task_tail->next = task;
+    task->prev = task_tail;
+    task_tail = task;
+
+out:
+    if(ISERR(res))
+    {
+        task_free(task);
+        return ERROR(res);
+    }
+    return task;
+}
+
+struct task* task_get_next()
+{
+    if(!current_task->next)
+    {
+        return task_head;
+    }
+    return current_task->next;
+}
+
+static void task_list_remove(struct task* task)
+{
+    if(task->prev)
+    {
+        /**
+         * detarch and link previous to next task
+         */
+        task->prev->next = task->next;
+    }
+    if(task == task_head)
+    {
+        /**
+         * case task is first
+         */
+        task_head = task->next;
+    }
+    if(task == task_tail)
+    {
+        /**
+         * case tail
+         */
+        task_tail = task->prev;
+    }
+    if(task == current_task)
+    {
+        current_task = task_get_next();
+    }
+}
+
+int task_free(struct task* task)
+{
+    paging_free_4gb(task->page_directory);
+    task_list_remove(task);
+
+    // free task data
+    kfree(task);
+    return 0;
+}
+
+int task_init(struct task* task)
+{
+    memset(task, 0, sizeof(struct task));
+
+    /**
+     * map entire 4gb space to its self
+     */
+    task->page_directory = paging_new_4gb(PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);;
+    if(!task->page_directory)
+    {
+        return -EIO;
+    }
+
+    task->registers.ip = SHEAROS_PROGRAM_VIRTUAL_ADDRESS;
+    task->registers.ss = USER_DATA_SEGMENT;
+    task->registers.esp = SHEAROS_PROGRAM_VIRTUAL_STACK_ADDRESS_START;
+
+    return 0;
+}
---
+++ src/task/task.h
@@ -0,0 +1,51 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <stdint.h>
+
+#include "config.h"
+#include "memory/paging/paging.h"
+
+struct registers
+{
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    uint32_t ip;
+    uint32_t cs;
+    uint32_t flags;
+    uint32_t esp;
+    uint32_t ss;
+};
+
+struct task
+{
+    /**
+     * page_directory_of the task
+     */
+    struct paging_4gb_chunk* page_directory;
+    /**
+     * registers of the task when task idle
+     */
+    struct registers registers;
+    /**
+     * next task in linked lisk
+     */
+    struct task* next;
+    /**
+     * previous task in llist
+     */
+    struct task* prev;
+};
+
+
+int task_free(struct task* task);
+struct task* task_new();
+struct task* task_get_next();
+struct task* task_current();
+
+#endif

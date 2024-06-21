0a7df0a89d6d0d830bbd832cdde4000140910aa5
Deo-EKodi <deoekodi@gmail.com>
2024-06-21 03:36:59 -0700

    called first kernel command - successful

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -63,7 +63,7 @@ void idt_init()
 
 void isr80h_register_command(int command_id, ISR80H_COMMAND command)
 {
-    if(command_id < 0 || command_id >= SHEAROS_MAX_ISR80H_COMMANDS)
+    if(command_id <  0 || command_id >= SHEAROS_MAX_ISR80H_COMMANDS)
     {
         panic("The command s out of bound!\n");
     }

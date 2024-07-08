4f4f1e5d0134c7713780678bf9072a151a1e93c4
Deo-EKodi <deoekodi@gmail.com>
2024-07-08 06:13:03 -0700

    broken strtok

--- programs/blank/src/blank.c
+++ programs/blank/src/blank.c
@@ -1,9 +1,20 @@
 #include "../../stdlib/src/shearos.h"
 #include "../../stdlib/src/stdlib.h"
 #include "../../stdlib/src/stdio.h"
+#include "../../stdlib/src/string.h"
+
 
 int main(int argc, char** argv)
 {
+    char words[] = "hello guy!!";
+    const char* token = strtok(words, " ");
+
+    while(token)
+    {
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
+    ;
     printf("\nOhh damn! printf just worked!\n");
 
     while(1){
